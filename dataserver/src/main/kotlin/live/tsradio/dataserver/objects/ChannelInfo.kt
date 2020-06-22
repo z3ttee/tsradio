e2309a823da293a4b570fef7c06e536e904be094
z3ttee <cedric.zitzmann@gmail.com>
2020-06-22 19:22:15 +0200

    Removed

--- dataserver/src/main/kotlin/live/tsradio/dataserver/objects/ChannelInfo.kt
+++
@@ -1,7 +0,0 @@
-package live.tsradio.dataserver.objects
-
-data class ChannelInfo(
-        val title: String,
-        val artist: String,
-        var history: HashMap<Long, HashMap<String, String>>?
-)
