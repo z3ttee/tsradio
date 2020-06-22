49c8efb7f3a2331468b30f7387b3e4ba81a154b8
z3ttee <cedric.zitzmann@gmail.com>
2020-06-22 11:56:32 +0200

    Authentication

--- dataserver/src/main/kotlin/live/tsradio/dataserver/listener/OnConnectListener.kt
+++ dataserver/src/main/kotlin/live/tsradio/dataserver/listener/OnConnectListener.kt
@@ -2,6 +2,10 @@ package live.tsradio.dataserver.listener
 
 import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.listener.ConnectListener
+import com.google.gson.JsonParser
+import live.tsradio.dataserver.api.InitialDataTransport
+import live.tsradio.dataserver.handler.AuthHandler
+import live.tsradio.dataserver.handler.ClientConnectionHandler
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -10,7 +14,23 @@ class OnConnectListener: ConnectListener {
 
     override fun onConnect(client: SocketIOClient?) {
         if(client != null) {
-            logger.info("Client '${client.remoteAddress}/${client.sessionId}' connected.")
+            val parser = JsonParser()
+
+            if(client.handshakeData.getSingleUrlParam("authenticate") != null) {
+                logger.info("Found authentication data on connect. Authenticating client...")
+                val data = parser.parse(client.handshakeData.getSingleUrlParam("authenticate"))
+                AuthHandler.authenticate(client, data.asJsonObject)
+            } else {
+                AuthHandler.authenticate(client, null)
+            }
+
+            if(AuthHandler.get(client.sessionId)!!.accountType == AuthHandler.AccountType.LISTENER) {
+                ClientConnectionHandler.add(client)
+                logger.info("Client '${client.remoteAddress}/${client.sessionId}' connected. Sending initial data...")
+                client.sendEvent("onInitialDataTransport", InitialDataTransport())
+            } else {
+                logger.info("Client '${client.remoteAddress}/${client.sessionId}' connected. Authenticated as daemon node")
+            }
         }
     }
 
