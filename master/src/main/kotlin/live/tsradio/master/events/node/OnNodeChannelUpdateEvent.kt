118b5a86ce787757ac29f657dc4e94f6dcc3e041
z3ttee <cedric.zitzmann@gmail.com>
2020-06-29 17:02:13 +0200

    Added channel removed event

--- master/src/main/kotlin/live/tsradio/master/events/node/OnNodeChannelUpdateEvent.kt
+++ master/src/main/kotlin/live/tsradio/master/events/node/OnNodeChannelUpdateEvent.kt
@@ -9,31 +9,48 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import live.tsradio.master.SocketServer
 import live.tsradio.master.handler.ClientHandler
+import live.tsradio.master.handler.RadioHandler
 import live.tsradio.master.packets.NodeChannelDataPacket
 import live.tsradio.master.utils.Events
 import live.tsradio.master.utils.Permissions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class OnNodeChannelUpdateEvent: DataListener<String> {
+    private val logger: Logger = LoggerFactory.getLogger(OnNodeChannelUpdateEvent::class.java)
 
     override fun onData(client: SocketIOClient?, data: String?, ackSender: AckRequest?) {
         if(client != null && data != null) {
+            logger.info("Channel update")
             val clientData = ClientHandler.getClient(client.sessionId)
 
             if(clientData != null) {
                 if (clientData.isNode) {
                     // Modify received packet (remove unnecessary data)
-                    val dataPacket = Gson().fromJson(data, NodeChannelDataPacket::class.java).toListenerSafeJSON()
+                    val dataPacket = Gson().fromJson(data, NodeChannelDataPacket::class.java)
+                    val oldData = RadioHandler.channels[dataPacket.channel.id]
 
-                    ClientHandler.clients.values.filter { !it.isNode }.forEach {
-                        it.client.sendEvent(Events.EVENT_NODE_CHANNEL_UPDATE, dataPacket)
+                    if(oldData!!.listed && !dataPacket.channel.listed) {
+                        // Not listed anymore -> Send removed event to clients
+                        ClientHandler.clients.values.filter { !it.isNode }.forEach {
+                            it.client.sendEvent(Events.EVENT_NODE_CHANNEL_REMOVED, "{\"id\": \"${dataPacket.channel.id}\"}")
+                        }
+                        return
                     }
 
+                    val clientSafe = dataPacket.toListenerSafeJSON()
+                    ClientHandler.clients.values.filter { !it.isNode }.forEach {
+                        it.client.sendEvent(Events.EVENT_NODE_CHANNEL_UPDATE, clientSafe)
+                    }
+
+                    RadioHandler.channels[dataPacket.channel.id] = dataPacket.channel
                 } else if(clientData.hasPermission(Permissions.PERMISSION_POST_CHANNEL_UPDATE)) {
                     // Check if client has permission to post channel updates
                     // If true -> Send update to matching node server
 
                     val dataPacket = Gson().fromJson(data, NodeChannelDataPacket::class.java)
                     ClientHandler.getNodeForChannel(dataPacket.channel)?.client?.sendEvent(Events.EVENT_NODE_CHANNEL_UPDATE, dataPacket.toJSON())
+                    RadioHandler.channels[dataPacket.channel.id] = dataPacket.channel
                 }
             }
         }
--- master/src/main/kotlin/live/tsradio/master/utils/Events.kt
+++ master/src/main/kotlin/live/tsradio/master/utils/Events.kt
@@ -2,4 +2,5 @@ package live.tsradio.master.utils
 
 object Events {
     const val EVENT_NODE_CHANNEL_UPDATE = "onNodeChannelUpdate"
+    const val EVENT_NODE_CHANNEL_REMOVED = "onNodeChannelRemoved"
 }
