1895ab2c7c7569bb2ef3954b818e4f2e032b4c97
z3ttee <cedric.zitzmann@gmail.com>
2020-06-30 19:24:04 +0200

    Centralised toJSON

--- master/src/main/kotlin/live/tsradio/master/packets/Packet.kt
+++ master/src/main/kotlin/live/tsradio/master/packets/Packet.kt
@@ -1,8 +1,12 @@
 package live.tsradio.master.packets
 
+import com.google.gson.Gson
+
 abstract class Packet {
 
     abstract fun toListenerSafeJSON(): String
-    abstract fun toJSON(): String
+    fun toJSON(): String {
+        return Gson().toJson(this)
+    }
 
 }
