01934b297d050a9d2c4e022a6a00103781758876
z3ttee <cedric.zitzmann@gmail.com>
2020-06-30 19:23:32 +0200

    Optimised

--- master/src/main/kotlin/live/tsradio/master/api/node/NodeChannelInfo.kt
+++ master/src/main/kotlin/live/tsradio/master/api/node/NodeChannelInfo.kt
@@ -1,12 +1,17 @@
 package live.tsradio.master.api.node
 
 import com.google.gson.annotations.Expose
+import live.tsradio.master.packets.Packet
 import java.util.*
 import kotlin.collections.HashMap
 
 data class NodeChannelInfo(
-        val id: UUID,
-        @Expose val title: String,
-        @Expose val artist: String,
-        @Expose val history: HashMap<Long, HashMap<String, String>>
-)
+    val id: UUID,
+    @Expose val title: String,
+    @Expose val artist: String,
+    @Expose val history: HashMap<Long, HashMap<String, String>>
+): Packet() {
+    override fun toListenerSafeJSON(): String {
+        TODO("Not yet implemented")
+    }
+}
