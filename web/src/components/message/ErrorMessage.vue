6f0c3990733fd008a14262d0874af9a994b10220
z3ttee <cedric.zitzmann@gmail.com>
2020-07-05 23:41:42 +0200

    Added

---
+++ web/src/components/message/ErrorMessage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="tsr_msg error">
+        <div class="msg_icon"><img src="/assets/images/icons/close.svg"></div>
+        <div class="msg_content">
+            <p>{{ error }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
