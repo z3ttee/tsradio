d149e767cd7a6d9cf72bff5ffc899403eafaf37c
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-10-17 14:27:48 +0200

    Added announcement to home page

---
+++ app/src/components/message/AppAnnouncementBoxComp.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="announcement-box">
+        <slot></slot>
+        <v-lottie-player class="animation-wrapper" width="256px" height="256px" loop autoplay :animationData="AnnouncementAnimData"></v-lottie-player>
+    </div>
+</template>
+
+<script>
+import AnnouncementAnimData from '@/assets/animated/announcement.json'
+
+export default {
+    props: {
+        content: String
+    },
+    data() {
+        return {
+            AnnouncementAnimData
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+
+.announcement-box {
+    position: relative;
+    display: block;
+    background-color: rgba($color: $colorPlaceholder, $alpha: 0.8);
+    border-radius: $borderRadSmall;
+    box-shadow: $shadowNormal;
+    min-height: 200px;
+    padding: $windowPad;
+    margin-top: 5em;
+    padding-left: 17em;
+
+    .animation-wrapper {
+        display: inline-block;
+        width: 500px;
+        position: absolute;
+        top: -40%;
+        left: 0;
+    }
+}
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App)
-.use(store)
-.use(router)
-.mount('#wrapper')
+import VueLottiePlayer from 'vue-lottie-player'
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(VueLottiePlayer)
+
+app.mount('#wrapper')
