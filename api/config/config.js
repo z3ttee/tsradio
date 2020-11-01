45b70f8ab02caab05da46425e63b244e697fd521
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-01 12:48:15 +0100

    Better routing

---
+++ api/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    app: {
+        devmode: true
+    },
+    ports: {
+        default: 3000,
+        ssl: 3443
+    }
+}
--- api/router/index.js
+++ api/router/index.js
@@ -1,18 +1,53 @@
-import express from 'express'
+import { ApiError } from '../models/error.js'
+import routes from './routes.js'
 
-const router = express.Router()
+class Router {
 
-//console.log(__dirname)
+    constructor() {
+        this.endpoints = routes
+        this.routes = {}
+    }
 
-router.get('/api', (req, res) => {
-    console.log(req.originalUrl)
-    res.json({
-        status: "ok"
-    })
-})
-router.get('*', (req, res) => {
-    res.send('Content soon...')
-    res.end()
-})
+    use(app) {
+        this.app = app
 
-export default router
+        for(var endpoint of this.endpoints) {
+
+            for(var route of endpoint.paths) {
+
+                if(!route.method) route.method = 'GET'
+            
+                if(this.routes[route.name.toLowerCase()]) {
+                    console.warn('> WARN: A route named ['+route.name.toLowerCase()+'] already exists! Ensure that each route is unique!')
+                } else {
+                    this.routes[route.name.toLowerCase()] = route
+                }
+
+                this.app[route.method.toLowerCase()](route.path, (req, res) => {
+                    const handler = endpoint.handler
+                    // Refactor action to match function naming scheme
+                    const action = 'action'+route.action.charAt(0).toUpperCase()+route.action.substr(1, route.action.length)        
+                    
+                    this.currentRoute = {...route, req, res}
+                    handler[action](this.currentRoute).then((result) => {
+                        res.setHeader('Content-Type', 'application/json');
+
+                        if(!result) {
+                            res.end(JSON.stringify({}))
+                        } else {
+                            if(result instanceof ApiError) {
+                                res.end(JSON.stringify(result.getAsJSON()))
+                            } else {
+                                res.end(JSON.stringify(result))
+                            }
+                        }
+                        
+                    })
+                })
+            }
+        }
+    }
+
+}
+
+export default new Router()
