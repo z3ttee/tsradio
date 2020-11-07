673182a5bbe4839226afd1e6b2fe98f802dd9628
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-07 19:15:10 +0100

    centralized user dbmodel

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,39 @@
-import { Sequelize,Model } from 'sequelize'
+import { Sequelize, Model, DataTypes } from 'sequelize'
+import config from '../config/config.js'
 
 class User extends Model {
-
-    async signInWithCredentials(username, password) {
-
+    hasPermission(permission) {
+        
     }
 }
 
-export default User
+const dbModel = {
+    uuid: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    groupUUID: {
+        type: DataTypes.UUID,
+        allowNull: true,
+    }
+}
+
+const dbSettings = {
+    tableName: config.mysql.prefix+'users',
+    timestamps: true,
+    updatedAt: false
+}
+
+export { User, dbModel, dbSettings }
