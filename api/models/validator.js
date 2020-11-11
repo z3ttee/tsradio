678c5a88ad78b23f8931e122b3cc8a87ba338afa
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-11 15:32:15 +0100

    implemented password validation

--- api/models/validator.js
+++ api/models/validator.js
@@ -1,3 +1,5 @@
+const passwordComplexity = require("joi-password-complexity");
+
 class Validator {
 
     static async validate(joiSchema, inputs) {
@@ -19,6 +21,35 @@ class Validator {
         }
 
         return { passed, error }
+    }
+
+    static async validatePassword(input) {
+        let passed = false
+        let error = undefined
+
+        let validatedPass = passwordComplexity({
+            min: 6,
+            max: 32,
+            lowerCase: 1,
+            upperCase: 1,
+            numeric: 1,
+            symbol: 1,
+            requirementCount: 4
+        }).validate(input)
+
+        if(validatedPass.error) {
+            error = {
+                err: "API_VALIDATION_ERROR",
+                code: 400,
+                message: validatedPass.error.details[0].message,
+                type: validatedPass.error.details[0].type.split(".", 1)[1],
+                path: "password"
+            }
+        } else {
+            passed = true
+        }
+
+        return { passed, error, value: input }
     }
 
 }
