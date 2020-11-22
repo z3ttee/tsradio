6ce4f16e806a352d2301b425c82965d72b725d0f
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-22 09:49:59 +0100

    channels now registering itself in redis

--- streamer/src/main/java/live/tsradio/streamer/listener/ChannelEventListener.java
+++ streamer/src/main/java/live/tsradio/streamer/listener/ChannelEventListener.java
@@ -1,20 +1,27 @@
 package live.tsradio.streamer.listener;
 
 import live.tsradio.streamer.database.Redis;
-import live.tsradio.streamer.database.RedisChannels;
+import live.tsradio.streamer.database.consts.RedisChannels;
+import live.tsradio.streamer.database.consts.RedisSets;
 import live.tsradio.streamer.objects.Channel;
 
 public class ChannelEventListener {
 
     public static void onChannelStarted(Channel channel) {
         String jsonData = "{ \"uuid\": \""+channel.getUuid()+"\", \"active\": \"true\"}";
+
+        Redis.getInstance().addToSet(RedisSets.SET_ACTIVE_CHANNELS, channel.getUuid());
         Redis.getInstance().publish(RedisChannels.CHANNEL_STATUS_UPDATE, jsonData);
+
         channel.logger.info("onChannelStarted(): Channel started.");
     }
 
     public static void onChannelStopped(Channel channel) {
         String jsonData = "{ \"uuid\": \""+channel.getUuid()+"\", \"active\": \"false\"}";
+
+        Redis.getInstance().removeFromSet(RedisSets.SET_ACTIVE_CHANNELS, channel.getUuid());
         Redis.getInstance().publish(RedisChannels.CHANNEL_STATUS_UPDATE, jsonData);
+
         channel.logger.info("onChannelStopped(): Channel stopped.");
     }
 
