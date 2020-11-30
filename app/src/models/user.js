1f6dfae2e0ec1634c550e336cdfe01d9e2d94cc3
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-30 16:22:13 +0100

    implemented auth into user model

--- app/src/models/user.js
+++ app/src/models/user.js
@@ -1,9 +1,13 @@
 import { VueCookieNext } from 'vue-cookie-next'
 import store from '@/store/index.js'
+import api from '@/models/api.js'
+import router from '@/router/index.js'
+
+const sessionCookieName = "tsr_session"
 
 class User {
     constructor(){
-        let token = VueCookieNext.getCookie("tsr_session")
+        let token = VueCookieNext.getCookie(sessionCookieName)
         store.state.user.isLoggedIn = token != undefined
         store.state.user.token = token
     }
@@ -12,8 +16,41 @@ class User {
         return store.state.user.token != undefined && store.state.user.isLoggedIn
     }
 
-    async loginWithCredentials(){
-        
+    async loginWithCredentials(username, password){
+        await api.post('/auth/signin', { username, password }).then((response) => {
+            if(response.status == 200) {
+                this.setToken(response.data.token)
+            }
+        });
+    }
+
+    async loginWithToken(){
+        let token = VueCookieNext.getCookie(sessionCookieName)
+        if(token) {
+            // TODO: Check if token is valid
+        } else {
+            this.logout()
+        }
+    }
+
+    async logout() {
+        this.setToken(null)
+        if(router.currentRoute.name != 'login') router.push({name: 'login'})
+    }
+
+    async setToken(value) {
+        if(value) {
+            store.state.user.token = value
+            store.state.user.isLoggedIn = true
+            VueCookieNext.setCookie(sessionCookieName, value, {
+                expire: "7d",
+                sameSite: "Lax"
+            })
+        } else {
+            store.state.user.token = undefined
+            store.state.user.isLoggedIn = false
+            VueCookieNext.removeCookie(sessionCookieName)
+        }
     }
 }
 
