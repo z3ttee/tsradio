a893d4db0eae32061360ae347498b772d7ac9914
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-11-30 18:11:06 +0100

    Removed

--- app/src/router/routes.js
+++
@@ -1,5 +0,0 @@
-export default [
-    { name: 'home', path: '/', component: () => import('@/views/pages/default/HomePageView.vue') },
-    { name: 'login', path: '/login', component: () => import('@/views/pages/default/LoginPageView.vue') },
-    { name: 'setup', path: '/setup', component: () => import('@/views/pages/setup/SetupIndexView.vue') }
-]
