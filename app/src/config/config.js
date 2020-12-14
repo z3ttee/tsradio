ff19115b62af6945504c2e27444e05d289c5f6af
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2020-12-14 14:15:59 +0100

    Cleanup

---
+++ app/src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+    api: {
+        baseURL: "http://localhost:3000/", //https://easternexploration.de:3000/
+        streamBase: "https://easternexploration.de:8000/"
+    },
+    session: {
+        cookieName: "tsr_session"
+    }
+}
