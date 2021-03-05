e8c5d162f53a7194c92a2fbdc83b89ba0f064144
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-03-05 19:03:42 +0100

    Fixed: Channel gets recognized when info update, after reconnect

--- streamer-v2/src/main/java/eu/tsalliance/streamer/listener/SocketOnConnectListener.java
+++ streamer-v2/src/main/java/eu/tsalliance/streamer/listener/SocketOnConnectListener.java
@@ -1,14 +1,18 @@
 package eu.tsalliance.streamer.listener;
 
-import eu.tsalliance.streamer.Core;
+import eu.tsalliance.streamer.channel.ChannelHandler;
 import io.socket.emitter.Emitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SocketOnConnectListener implements Emitter.Listener {
+    private static final Logger logger = LoggerFactory.getLogger(SocketOnConnectListener.class);
 
     @Override
     public void call(Object... args) {
-
+        logger.info("Connected to socket");
+        ChannelHandler.notifyAllChannelStates();
+        ChannelHandler.notifyAllChannelInfos();
+        ChannelHandler.notifyAllChannelHistories();
     }
 }
