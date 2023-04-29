b7482e089a6c3520f0f5cada3ef1397cdc36015c
Cedric <cedric.zitzmann@gmail.com>
2023-04-29 10:22:19 +0200

    added admin layout

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SSOGuard } from './modules/sso/guards/sso.guard';
 import { MainLayoutComponent } from './layouts/main/main-layout.component';
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AdminLayoutModule } from './layouts/admin/admin-layout.module';
 
 const routes: Routes = [
+  { path: "admin", component: AdminLayoutComponent, canActivate: [SSOGuard], children: [
+    { path: "", canActivate: [SSOGuard], loadChildren: () => import("./modules/channel/channel.module").then((m) => m.ChannelModule) },
+  ]},
   { path: "", component: MainLayoutComponent, canActivate: [SSOGuard], children: [
     { path: "", canActivate: [SSOGuard], loadChildren: () => import("./modules/channel/channel.module").then((m) => m.ChannelModule) },
-  ]}
+  ]},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    AdminLayoutModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ app/src/app/layouts/admin/admin-layout.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NgIconsModule } from "@ng-icons/core";
+import { NavListItemComponent } from "src/app/components/nav-list-item/nav-list-item.component";
+import { AdminLayoutComponent } from "./admin-layout.component";
+import { heroSignalSolid } from "@ng-icons/heroicons/solid"
+import { heroRectangleGroup } from "@ng-icons/heroicons/outline"
+
+@NgModule({
+    declarations: [
+        AdminLayoutComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        NgIconsModule.withIcons({ heroSignalSolid, heroRectangleGroup }),
+        NavListItemComponent
+    ],
+    exports: [
+        AdminLayoutComponent
+    ]
+})
+export class AdminLayoutModule {}
