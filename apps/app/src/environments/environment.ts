2c140d84e8ba459a74b674854867928d59095388
z3ttee <cedric.zitzmann@gmail.com>
2024-05-30 17:05:26 +0200

    updated api url

--- apps/app/src/environments/environment.ts
+++ apps/app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    api_base_uri: "https://api.tsalliance.eu/tsradio",
-    keycloak_url: "https://sso.tsalliance.eu/",
+    api_base_uri: "https://api.zitzmann.dev/tsradio",
+    keycloak_url: "https://sso.zitzmann.dev/",
     keycloak_realm: "tsalliance",
     keycloak_client_id: "alliance-tsradio-app",
     admin_role: "admin",
